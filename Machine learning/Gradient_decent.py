5308c755ea48b4223cd8a66bae1d2eeb7447c2f0
Ravi kumar <109415623+Ravikumarchavva@users.noreply.github.com>
2022-11-13 19:48:26 +0000

    commited

--- Machine learning/Gradient_decent.py
+++ Machine learning/Gradient_decent.py
@@ -1,10 +1,11 @@
 import numpy as np
 
+
 def gradient_descent(x,y):
     m_curr = b_curr = 0
     iterations = 10000
     n = len(x)
-    learning_rate = 0.08
+    learning_rate = 0.008
 
     for i in range(iterations):
         y_predicted = m_curr * x + b_curr
@@ -15,7 +16,7 @@ def gradient_descent(x,y):
         b_curr = b_curr - learning_rate * bd
         print ("m {}, b {}, cost {} iteration {}".format(m_curr,b_curr,cost, i))
 
-x = np.array([1,2,3,4,5])
-y = np.array([5,7,9,11,13])
+a = np.array([1,2,3,4,5])
+b = np.array([6,10,14,18,22])
 
-gradient_descent(x,y)
+gradient_descent(a,b)
